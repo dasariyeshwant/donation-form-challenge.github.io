3a114d0fb8f60000fc43ed2e4f4ecd28d0827855
Yeshwant <ydasari@forddirect.com>
2021-01-12 02:12:36 -0500

    Adds functionality to submit donation amount

--- src/components/donationCenter/DonationForm/DonationForm.js
+++ src/components/donationCenter/DonationForm/DonationForm.js
@@ -1,24 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CardContainer from '../../common/cardContainer/CardContainer';
 import './DonationForm.scss';
+import { isAllNumberic } from '../../../utils/Utils';
 
-const DonationForm = () => {
+const DonationForm = ({handleClick, totalDonors, totalAmountDonated, totalGoal}) => {
+    const [formData, setFormData] = useState({
+        donationAmount: '',
+        hasError: false,
+        isSubmitted: false,
+    });
+    const progressBarWidth = totalAmountDonated <= totalGoal ? `${totalAmountDonated / 50}%` : '100%';
+    const changeHandler = (event) => {
+        if (isAllNumberic(event.target.value) || !event.target.value) {
+            setFormData({...formData, donationAmount: event.target.value})
+            // setDonationAmount(event.target.value);
+        }
+    }
+    const handleSubmit = () => {
+        if (parseInt(formData.donationAmount) < 5) {
+            // setHasError(true);
+            setFormData({
+                ...formData,
+                hasError: true,
+                isSubmitted: true
+            });
+        } else {
+            handleClick(formData.donationAmount);
+            setFormData({
+                ...formData,
+                hasError: false,
+                donationAmount: '',
+                isSubmitted: true
+            });
+        }
+    }
     return (
         <div className="donation-form">
             <CardContainer className="donation-form__card">
                 <div className="donation-form__progress-bar-container">
-                    <div className="donation-form__progress-bar"></div>
+                    <div className="donation-form__progress-bar" style={{width: progressBarWidth}}></div>
                 </div>
                 <h1 className="donation-form__header">
                     Only four days left to fund this project
                 </h1>
                 <p className="donation-form__description">
-                    Join the <span className="donation-form__description_bold">11</span> other donors who have already supported this project.
+                    Join the <span className="donation-form__description_bold">{totalDonors}</span> other donors who have already supported this project.
                 </p>
-                <div className="donation-form__input-container">
+                { formData.hasError && (
+                    <p className="donation-form__error">
+                        Sorry, We cannot accept donations of less than $5
+                    </p>
+                )
+                }
+                { !formData.hasError && formData.isSubmitted && (
+                    <p className="donation-form__success">
+                        Thank You very much for your donation.
+                    </p>
+                )
+                }
+                <div className={`donation-form__input-container ${formData.hasError && formData.isSubmitted ? 'has-error' : 'has-success'}`}>
                     <div className="donation-form__input-currency-symbol">$</div>
-                    <input className="donation-form_input-field"/>
-                    <button className="donation-form__submit">Give Now</button>
+                    <input className="donation-form_input-field" value={formData.donationAmount} name="donationAmountField" onChange={changeHandler}/>
+                    <button className="donation-form__submit" onClick={handleSubmit}>Give Now</button>
                 </div>
             </CardContainer>
         </div>
